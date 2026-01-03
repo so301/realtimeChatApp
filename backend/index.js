1d6919c5ec3888c5cc0147e421653552a81c5f8c
so301 <fukesopan880@gmail.com>
2026-01-03 22:46:42 +0530

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtimechatapp-kgoj.onrender.com",
     credentials:true
 }))
 app.use(express.json())
